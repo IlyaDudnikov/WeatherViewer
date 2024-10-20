1bcac4d71d3942cd6108555741516b4f0a2346f3
ilyad <iladudnikov13@gmail.com>
2024-10-20 20:13:53 +0700

    setup: basic Spring configuration with Thymeleaf

---
+++ src/main/java/com/ilyadudnikov/weatherViewer/config/SpringConfig.java
@@ -0,0 +1,44 @@
+package com.ilyadudnikov.weatherViewer.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+@ComponentScan("com.ilyadudnikov.weatherViewer")
+@EnableWebMvc
+@EnableTransactionManagement
+public class SpringConfig implements WebMvcConfigurer {
+    @Bean
+    public SpringResourceTemplateResolver templateResolver() {
+        SpringResourceTemplateResolver templateResolver = new SpringResourceTemplateResolver();
+        templateResolver.setPrefix("/WEB-INF/views/");
+        templateResolver.setSuffix(".html");
+        templateResolver.setTemplateMode("HTML5");
+        return templateResolver;
+    }
+
+    @Bean
+    public SpringTemplateEngine templateEngine() {
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(templateResolver());
+        return templateEngine;
+    }
+
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        ThymeleafViewResolver thymeleafViewresolver = new ThymeleafViewResolver();
+        thymeleafViewresolver.setCharacterEncoding("UTF-8");
+        thymeleafViewresolver.setTemplateEngine(templateEngine());
+        registry.viewResolver(thymeleafViewresolver);
+    }
+
+
+}
